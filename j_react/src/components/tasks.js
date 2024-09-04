f4e952a4a9d16ec4002917113da00bc743ef6625
Sharnu Ravindra Narsagol <sharanuvishaln@gmail.com>
2024-09-04 17:36:36 +0530

    Created card in task comp.

--- j_react/src/components/tasks.js
+++ j_react/src/components/tasks.js
@@ -1,3 +1,28 @@
 export default function Tasks() {
-  return <div>Tasks</div>;
+  const cardData = {
+    headerText: "2 min",
+    title: "short title",
+    description: "short description",
+  };
+
+  return (
+    <>
+      <h1>Tasks</h1>
+      <div className="card">
+        <div className="card-header"> {cardData.headerText}</div>
+
+        <div className="card-body">
+          <div className="d-flex justify-content-around">
+            <h5 className="card-title">{cardData.title}</h5>
+
+            <button className="btn btn-primary">Add</button>
+          </div>
+
+          <hr />
+
+          <p className="card-text">{cardData.description}</p>
+        </div>
+      </div>
+    </>
+  );
 }
