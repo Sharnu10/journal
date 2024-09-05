adda593610489162d0bf0094ff4159e1c742c572
Sharnu Ravindra Narsagol <sharanuvishaln@gmail.com>
2024-09-05 17:26:00 +0530

    Created api & Added proxy & cors
    - Created api with useEffect.
    - Added proxy to package in react 18 app.
    - Added cors to express app.

--- j node/index.ts
+++ j node/index.ts
@@ -1,20 +1,27 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
 
-import {taskRouter} from './src/router/task-router';
-import { db } from './src/sqlite/config/db.config';
-import { cardRoutes } from './src/router/card-router';
+import { taskRouter } from "./src/router/task-router";
+import { db } from "./src/sqlite/config/db.config";
+import { cardRoutes } from "./src/router/card-router";
 
 const app = express();
 const port = 3003;
 
 db;
 
-app.get('/', (req, res) => {
-  res.send('server is up');
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("server is up");
 });
 
-app.use('/api', taskRouter);
-app.use('/api/card', cardRoutes);
+app.use("/api", taskRouter);
+app.use("/api/card", cardRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- j_react/src/components/card.js
+++ j_react/src/components/card.js
@@ -1,9 +1,41 @@
+import { useEffect, useState } from "react";
+
 import "../styles/utility.scss";
 
 export default function Card({ cardData, cardStyle }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
   const getPriorityColor = (cardData) => {
     return `card-header priority-${cardData.priority}`;
   };
+
+  const apiUrl = "http://localhost:3003/api/card";
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (data) return <div>{JSON.stringify(data, null, 2)}</div>;
 
   return (
     <div className="card m-1" style={{ width: cardStyle.width }}>
