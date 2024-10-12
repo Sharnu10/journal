1968897d29726668cddd8171c80a032f25bc6c1f
Sharnu Ravindra Narsagol <sharanuvishaln@gmail.com>
2024-10-12 08:30:15 +0530

    Created Error Handling
    - Created errorHandler file for 'Centerlized Error Handling'.
    - Created baseError class for all 'custom error'.
    - Created error status code.

--- j node/index.ts
+++ j node/index.ts
@@ -5,6 +5,11 @@ import { taskRouter } from "./src/router/task-router";
 import { db } from "./src/sqlite/config/db.config";
 import { cardRouter } from "./src/router/card-router";
 import { formikRouter } from "./src/router/formik-router";
+import {
+  errorHandler,
+  logError,
+  returnError,
+} from "./src/middleware/errorHandler";
 
 const app = express();
 const port = 3003;
@@ -38,6 +43,23 @@ app.get("/", (req, res) => {
 app.use("/api/task", taskRouter);
 app.use("/api/card", cardRouter);
 app.use("/api/formik", formikRouter);
+
+app.use(errorHandler);
+app.use(logError);
+app.use(returnError);
+
+// Global error handling for unhandled promise rejections and uncaught exceptions
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
+  // Optional: send entire application down. Recommended to restart the server in production.
+  process.exit(1);
+});
+
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception thrown:", error);
+  // Optional: send entire application down. Recommended to restart the server in production.
+  process.exit(1);
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
---
+++ j node/src/middleware/errorHandler.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express";
+import BaseError from "./error/baseError";
+
+// Log the error
+function logError(err: Error, req: Request, res: Response, next: NextFunction) {
+  console.error(err);
+  next(err); // Pass the error to the next middleware
+}
+
+// Return error response to the client
+function returnError(err: Error, req: Request, res: Response, next: any) {
+  res.status(500).json({
+    message: err.message || "Internal Server Error",
+  });
+}
+
+// Catch-all error handler
+function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.log("Centerlized Error Handling: ", err.message);
+
+  res.status(500).json({
+    message: "Internal Server Error",
+    error: err.message || "An unexpected error occurred",
+  });
+}
+
+function isOperationalError(error: Error) {
+  if (error instanceof BaseError) {
+    return error.isOperational;
+  }
+  return false;
+}
+
+export { errorHandler, logError, returnError, isOperationalError };
