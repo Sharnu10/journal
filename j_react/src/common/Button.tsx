39ce26ec380968c1ca19488f679297207a04381f
Sharnu Ravindra Narsagol <sharanuvishaln@gmail.com>
2024-10-14 22:48:24 +0530

    Updated to take generic props in Button

--- j_react/src/common/Button.tsx
+++ j_react/src/common/Button.tsx
@@ -1,7 +1,19 @@
-const Button = (props: any) => {
+import { FC, MouseEventHandler } from "react";
+
+interface ButtonProps {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  label?: string;
+  className?: string;
+}
+
+const Button: FC<ButtonProps> = ({
+  onClick,
+  label = "click",
+  className = "btn btn-primary",
+}) => {
   return (
-    <button className="btn btn-primary" onClick={props.callApi}>
-      Click to generate
+    <button className={className} onClick={onClick}>
+      {label}
     </button>
   );
 };
