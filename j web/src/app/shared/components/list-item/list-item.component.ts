948cf8d7a37f241929d7cdbadfa50f8a7b4aaf15
Sharnu10 <sharanuvishaln@gmail.com>
2024-11-01 22:42:24 +0530

    Optimized for production
    - Increased configuration value for warning & error.
    - Removed moment package.
    - Created TS code as alternative to moment package.

--- j web/src/app/shared/components/list-item/list-item.component.ts
+++ j web/src/app/shared/components/list-item/list-item.component.ts
@@ -1,9 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import moment from 'moment';
 
-import { ITask } from '../../model/task.model';
 import { TooltipDirective } from '../../directive/tooltip/tooltip.directive';
+import { ITask } from '../../model/task.model';
 
 @Component({
   selector: 'app-list-item',
@@ -24,15 +23,32 @@ export class ListItemComponent {
   }
 
   getDateDiff(task: ITask) {
-    const todayDate = moment();
+    const todayDate =  new Date();
     const createdDate = task.createdDate;
-    const diff = todayDate.diff(createdDate, 'days');
+    const differenceIn = this.dateDifference(todayDate, createdDate);
 
-    if (diff < 0) {
-      return 'border-right-low';
-    } else if (diff > 0) {
-      return 'border-right-high';
-    }
-    return '';
+   
+
+    return differenceIn.days <= 0 ? 'border-right-low' : 'border-right-high';
+    
+  }
+
+  dateDifference(date1: Date, date2: Date) {
+    // Get the absolute difference in milliseconds
+    const differenceInMs = Math.abs(date2.getTime() - date1.getTime());
+  
+    // Calculate days
+    const days = Math.floor(differenceInMs / (1000 * 60 * 60 * 24));
+    
+    // Calculate hours
+    const hours = Math.floor((differenceInMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    
+    // Calculate minutes
+    const minutes = Math.floor((differenceInMs % (1000 * 60 * 60)) / (1000 * 60));
+    
+    // Calculate seconds
+    const seconds = Math.floor((differenceInMs % (1000 * 60)) / 1000);
+  
+    return { days, hours, minutes, seconds };
   }
 }
